ae8431e446e1113a84717fe80feda9897b86e47d
Paul Calnan <pcalnan71@gmail.com>
2022-03-20 14:08:02 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      avatar1: {
+        name: 'Paul'
+      },
+      avatar2: {
+        name: 'Mack'
+      },
+      avatar3: {
+        name: 'Tiff'
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.avatar1.name}</h1>
+        <h1>{this.state.avatar2.name}</h1>
+        <h1>{this.state.avatar3.name}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
