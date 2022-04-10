259f0b3cf77e1206e3f9a275780e433476efad48
Paul Calnan <pcalnan71@gmail.com>
2022-04-11 04:46:00 +1000

    format code

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -7,10 +7,9 @@ class Card extends Component {
   render() {
     //destructuring from props
     const { id, name, email } = this.props.avatar;
-    const [firstName, lastName] = name.split(' ');
+    const [firstName, lastName] = name.split(" ");
     
-    return (
-     
+    return (  
       //Use key id to prevent react warning that each child should have a unique key prop
       <div className='card-container' key={id}>
         <img 
