2c83cb185e6e0fd7a04c1cee18b2e11a5c9ef41b
Paul Calnan <pcalnan71@gmail.com>
2022-06-23 07:27:02 +1000

    repace cardlist and searchbox with functional components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,24 +1,14 @@
-import { Component } from 'react';
 import Card from '../card/card.component';
 import './card-list.styles.css';
 
-// initialise CardList class
-class CardList extends Component {
-  render() {
-    //destructuring from props
-    const { avatars } = this.props;
-
-    return (
-      // All component html elements must be contained in one parent level tag
-      <div className='card-list'>
-        {avatars.map((avatar) => {
-          return(
-            <Card avatar={avatar} />
-          );
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ avatars }) => (
+  <div className='card-list'>
+    {avatars.map((avatar) => {
+      return(
+        <Card avatar={avatar} />
+      );
+    })}
+  </div>
+);
 
 export default CardList;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,25 +1,20 @@
-import { Component } from 'react';
-
 import './card.styles.css';
 
-// initialise CardList class
-class Card extends Component {
-  render() {
-    //destructuring from props
-    const { id, name, email } = this.props.avatar;
-    const [firstName, lastName] = name.split(" ");
-    
-    return (  
-      //Use key id to prevent react warning that each child should have a unique key prop
-      <div className='card-container' key={id}>
-        <img 
-          alt={`avatar ${name}`} 
-          src={`https://avatars.dicebear.com/api/adventurer/${id}.svg?size=180`} 
-        />
-        <h2 className='card-name'>{firstName}</h2>
-        <h2 className='card-name'>{lastName}</h2>
-        <p>{email}</p>
-      </div>
-    )}}
+const Card = ({ avatar }) => {  
+  const { id, name, email } = avatar;
+  const [firstName, lastName] = name.split(" ");
+
+  return (
+    <div className='card-container' key={id}>
+      <img 
+        alt={`avatar ${name}`} 
+        src={`https://avatars.dicebear.com/api/adventurer/${id}.svg?size=180`} 
+      />
+      <h2 className='card-name'>{firstName}</h2>
+      <h2 className='card-name'>{lastName}</h2>
+      <p>{email}</p>
+    </div>
+  );
+}
 
 export default Card;
